be068835146851dedf1331f50fb4a574ff9d86f3
Student <student@lan.itstep.kiev.ua>
2018-07-03 19:42:39 +0300

    Initial commit 3july2018

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+/**
+ * Created by student on 03.07.2018.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 1;
+        int k = 2;
+        String puppy = "puppy";
+        String puppy1 = "puppy1";
+        String puppy2 = "puppy2";
+        int result = i;
+        i++;
+        int result1 = i +j * k;
+        j++;
+        int result2 = i +j * k;
+        k++;
+        int result3 = i +j * k;
+
+        System.out.println("====>>>> Result i " + i + " = "+ result //0
+                        + "\n====>>>> Result1 j " + i + " = " + result1 //
+                        + "\n====>>>> Result2 j " + j + " = " + result2
+                        + "\n====>>>> Result3 k " + k+ " = " + result3
+                        + "\n====>>>> New result i++ = " + i++
+                        + "\n====>>>> New result ++i = " + ++i);
+//        System.out.println("\n====>>>> New result ++i = " + ++i);
+    }
+}
